65e6243e1e378d1f8a22830230f7e2eb07746fe2
Leksyking <gbemilekeogundipe@gmail.com>
2024-03-28 14:57:57 +0100

    create the create room and join room components and route them in app.jsx

--- client-react/src/App.jsx
+++ client-react/src/App.jsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import CreateRoom from "./components/CreateRoom";
+import Room from "./components/Room";
 
 function App() {
-        const [count, setCount] = useState(0);
+        return (
+                <div className="App">
+                        <BrowserRouter>
+                                <Switch>
+                                        <Route
+                                                path="/"
+                                                Component={CreateRoom}
+                                        ></Route>
+                                        <Route
+                                                path="/room/:roomID"
+                                                Component={Room}
+                                        ></Route>
+                                </Switch>
+                        </BrowserRouter>
+                </div>
+        );
 }
 
 export default App;
